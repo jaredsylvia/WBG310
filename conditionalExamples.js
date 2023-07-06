79f0452193f74298f2dbed623e250b57a9e5aa8c
jaredsylvia <jaredsylvia@gmail.com>
2023-07-05 17:36:49 -0700

    Create conditionalExamples.js

---
+++ conditionalExamples.js
@@ -0,0 +1,21 @@
+console.log(15 > 10); // returns true because 15 > 10, the condition has been met
+
+console.log("" === ""); // returns true because "" is exactly equal to "".
+
+
+let variable;
+console.log(typeof variable !== "undefined"); // returns false because variable is undefined and the condition that's being checked is wether the variable is not undefined
+
+
+console.log(7 > 5 && 7 < 10); // returns true because 7 is greater than 5 AND 7 is less than 10
+
+const text = "Hello, world!";
+console.log(text.includes("world")); // returns true because the variable contains the word "world" so the condition is met.
+
+
+const array = [1, 2, 3, 4, 5];
+console.log(array.includes(3)); // returns true because the array does invlude 3, again the condition is
+
+
+const number = 7;
+console.log(number === 5 || number === 10); // returns false because number does not equal 5 OR 10, condition is unmet.
